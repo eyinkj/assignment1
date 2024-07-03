3d87d2577a8b4e626cbead9360327c37d63e9d02
Jack <125064464+eyinkj@users.noreply.github.com>
2024-07-03 04:08:46 -0500

    Add files via upload

---
+++ microservices warm-up/prng.py
@@ -0,0 +1,5 @@
+while true:
+
+    sleep(1)
+    open("C:\Users\Jack Eyink\Desktop\microservices warm-up\prng-service.txt")
+    read()
---
+++ microservices warm-up/ui.py
@@ -0,0 +1,7 @@
+while True:
+
+    sleep(1)
+    x = input("IM A HACKER AND IM INSIDE YOUR DEVICE. CHOOSE THE CORRECT NUMBER AND YOUR INFORMATION WILL REMAIN UNTOUCHED. TYPE 0 OR 1: ")
+    if x == 0:
+        open("C:\Users\Jack Eyink\Desktop\microservices warm-up\prng-service.txt")
+        
